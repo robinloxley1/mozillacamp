c86a2ba9653fd22d11652a6ac88dd5c03fe3046a
U-ASIAPACIFIC\zhaoguop <zhaoguop@GZHAO15.asiapacific.hpqcorp.net>
2013-12-11 10:52:44 +0800

    Day1 work with messagebox using bootstrap

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ mozillacamp
 ===========
 
 temporary repo for boot camp by mozilla and ntu 2013
+
+
+# workflow
+1. on Windows develop in dev branch, using webstorm
+2. on Windows after test, commit to github
+3. on Linux	git fetch from dev, deploy to heroku
---
+++ mb-script.js
@@ -0,0 +1,39 @@
+function addMessage(message, target){
+    var p = document.createElement("p");
+    p.textContent = message;
+
+    var parent = target;
+    var newElement = p;
+    var referenceElement = parent.firstChild;
+    parent.insertBefore(newElement,referenceElement);
+
+    console.log(referenceElement);
+}
+
+function onSubmit() {
+    var input = document.getElementById("message_field");
+    var message = input.value;
+    console.log(message);
+    var target = document.getElementById("message_box");
+    input.value = null;
+    addMessage(message, target);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    var hello = document.getElementById("hello");
+    var button = document.getElementById("myButton");
+
+    button.addEventListener("click", onSubmit);
+
+//  hello.style.fontSize = "100px";
+//
+//  var closedOverCounter = 0;
+//
+//  function whenClicked (event) {
+//    console.log("Hey I was clicked " + ++closedOverCounter +
+//                " time[s]");
+//    hello.classList.toggle("valid");
+//  };
+
+//  button.addEventListener("click", whenClicked);
+});

91e9b1a7e2abb7e76f5410692739222465562194
U-ASIAPACIFIC\zhaoguop <zhaoguop@GZHAO15.asiapacific.hpqcorp.net>
2013-12-26 10:28:38 +0800

    test client side with bootstrap and deployable to firefox os

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
-mozillacamp
+Chat Leo, a side project from mozillacamp
 ===========
 
-temporary repo for boot camp by mozilla and ntu 2013
+repo for boot camp by mozilla and ntu 2013.
 
+It is a Client-Server chat application using node.js and javascript at client side.
+The main purpose is to demonstrate the use of html5 and bootstrap framework for web applications.
+The application is deployable to Firefox OS and tested on firefox os mobile devices.
 
-# workflow
+# Development workflow
 1. on Windows develop in dev branch, using webstorm
 2. on Windows after test, commit to github
 3. on Linux	git fetch from dev, deploy to heroku
+
+# Deploy to Heroku
--- mb-script.js
+++ mb-script.js
@@ -1,33 +1,121 @@
 function addMessage(message, target) {
     var p = document.createElement("p");
     p.textContent = message;
-
     var parent = target;
     var newElement = p;
     var referenceElement = parent.firstChild;
     parent.insertBefore(newElement, referenceElement);
-
-    console.log(referenceElement);
 }
 
-function onSubmit() {
+function addImage(from, src, target) {
+    var img = new Image();
+    img.onload = function () {
+        var p = document.createElement("p");
+        p.textContent = from;
+        target.insertBefore(img, target.firstChild);
+        target.insertBefore(p, target.firstChild);
+        Notification.requestPermission( function (){
+            new Notification(message.from, {icon:message.content});
+        });
+    };
+    img.src = src;
+
+}
+
+function addLink(from, link, target) {
+    var p = document.createElement("p");
+    p.textContent = from + " just shared a link as below";
+    var linkChild = document.createElement("a");
+    linkChild.setAttribute("href", link);
+    linkChild.innerHTML = link;
+    target.insertBefore(linkChild, target.firstChild);
+    target.insertBefore(p, target.firstChild);
+}
+function onSubmit(socket, type) {
+    var myName = document.getElementById("myName").textContent;
     var input = document.getElementById("message_field");
-    var message = input.value;
-    console.log(message);
     var target = document.getElementById("message_box");
+
+    if (type === "text") {
+        addMessage(myName + ": " + input.value, target);
+    } else if (type === "image") {
+        addImage(myName + ":", input.value, target);
+    } else if (type === "link") {
+        console.log(">>>>>> link!!");
+        addLink(myName + ":", input.value, target);
+    }
+
+    try {
+        socket.send(JSON.stringify({
+            type: type,
+            from: myName,
+            content: input.value
+        }));
+        console.log("no error from the server so I assume it is sent.");
+    } catch (err) {
+        console.error(err);
+    }
     input.value = null;
-    addMessage(message, target);
+}
+
+function onMessage(rawMessage) {
+    try {
+        var message = JSON.parse(rawMessage.data);
+    } catch (error) {
+        console.log(error);
+        return;
+    }
+
+    var messageBox = document.getElementById("message_box");
+    if (message.type === "text" && message.content.length < 50) {
+        addMessage(message.from + ":" + message.content, messageBox);
+        Notification.requestPermission( function (){
+            new Notification(message.from, {body:message.content});
+        });
+    } else if (message.type === "image") {
+        addImage(message.from + ":", message.content, messageBox);
+//        Notification.requestPermission( function (){
+//            new Notification(message.from, {icon:message.content});
+//        });
+    } else if (message.type === "link") {
+        addLink(message.from, message.content, messageBox);
+        Notification.requestPermission( function (){
+            new Notification(message.from, {body:message.content});
+        });
+    }
+}
+
+function onClose() {
+    console.log("socket closed");
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    var hello = document.getElementById("hello");
-    var button = document.getElementById("myButton");
+    var myName = document.getElementById("myName").textContent;
+    console.log(myName);
 
-    button.addEventListener("click", onSubmit);
+    var socket = new WebSocket("ws://172.22.131.203:3000");
+    console.log(socket);
+    socket.onmessage = onMessage;
+    socket.onclose = onClose;
+    console.log("socked loaded");
+
+//    button.addEventListener("click", onSubmit(socket));
+    window.addEventListener("beforeunload", function () {
+        socket.close();
+    });
+
+    document.getElementById("myButton").addEventListener("click", function () {
+        onSubmit(socket, "text");
+    });
+    document.getElementById("image_submit").addEventListener("click", function () {
+        onSubmit(socket, "image");
+    });
+
+    document.getElementById("link_submit").addEventListener("click", function () {
+        onSubmit(socket, "link");
+    });
 
     var iconTarget = document.getElementById("icon-target");
-
-//    iconTarget.addEventListener("")
 
     iconTarget.addEventListener("dragover", function (e) {
         console.log("drog over!");
@@ -35,19 +123,16 @@ document.addEventListener("DOMContentLoaded", function () {
         var imgTarget = document.getElementById("img-target")
         imgTarget.classList.add("mousein");
         iconTarget.classList.add("mousein");
-//        imgTarget.style.width="80%";
-//        imgTarget.style.height="80%";
-//        iconTarget.style.border="5px #e6e6e6 solid";
     });
 
-    iconTarget.addEventListener("dragleave", function (e){
+    iconTarget.addEventListener("dragleave", function (e) {
         console.log("drag leave!");
         var imgTarget = document.getElementById("img-target");
         imgTarget.classList.remove("mousein");
         iconTarget.classList.remove("mousein");
     });
 
-    iconTarget.addEventListener("drop", function(e){
+    iconTarget.addEventListener("drop", function (e) {
         console.log("drop!");
         e.preventDefault();
         e.stopPropagation();
@@ -59,11 +144,10 @@ document.addEventListener("DOMContentLoaded", function () {
         var file = e.dataTransfer.files[0];
         var fileReader = new FileReader();
         fileReader.readAsDataURL(file);
-        fileReader.onload = function (e){
+        fileReader.onload = function (e) {
             var imgTarget = document.getElementById("img-target");
             console.log(imgTarget.src);
-//            console.log(e.target.result);
-            imgTarget.src= e.target.result;
+            imgTarget.src = e.target.result;
         }
     });
 });
